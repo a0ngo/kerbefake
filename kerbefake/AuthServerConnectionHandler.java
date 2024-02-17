08d8b717ef35019a18e2e8e027d60dd0726bd991
a0ngo <106151463+a0ngo@users.noreply.github.com>
2024-02-17 23:35:38 +0200

    fix: updated connection handler to properly handle messages

--- kerbefake/AuthServerConnectionHandler.java
+++ kerbefake/AuthServerConnectionHandler.java
@@ -1,10 +1,8 @@
 package kerbefake;
 
-import kerbefake.errors.InvalidRequestException;
+import kerbefake.errors.InvalidMessageException;
+import kerbefake.models.auth_server.AuthServerMessage;
 import kerbefake.models.auth_server.requests.AuthServerRequest;
-import kerbefake.models.auth_server.requests.AuthServerRequestBody;
-import kerbefake.models.auth_server.AuthServerRequestHeader;
-import kerbefake.models.auth_server.RequestCode;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -28,7 +26,7 @@ public class AuthServerConnectionHandler implements Runnable {
     @Override
     public void run() {
         BufferedInputStream in;
-        BufferedOutputStream out = null;
+        BufferedOutputStream out;
         try {
             in = new BufferedInputStream(conn.getInputStream());
             out = new BufferedOutputStream(conn.getOutputStream());
@@ -38,76 +36,37 @@ public class AuthServerConnectionHandler implements Runnable {
         }
 
         while (true) {
-            AuthServerRequestHeader header = this.readHeader(in);
-            AuthServerRequestBody body = null;
-            if(header == null){
-                continue;
-            }
-            if(header.getPayloadSize() != 0){
-                body = this.readBody(in, header.getPayloadSize(), header.getCode());
-                if(body == null){
-                    continue;
-                }
-            }
-
             try {
-                AuthServerRequest.buildFor(header, body);
-            } catch (InvalidRequestException e) {
-                error("Failed to build request: %s", e);
-                continue;
-            }
+                AuthServerMessage message = AuthServerMessage.parse(in);
 
+                // On this specific class we always know that we expect messages that are requests, if there's an issue we simply close the connection;
+                // Because we do actually allow the parsing of a response message here.
+                // This is why we hvae a catch for classcastexception below.
+                AuthServerRequest req = (AuthServerRequest) message;
+                AuthServerMessage res = req.execute();
+
+                out.write(res.toLEByteArray());
+                out.flush();
+
+
+            } catch (InvalidMessageException | ClassCastException e) {
+                // This is just an invalid message, or one we don't know how to handle - ignore and close connection.
+                e.printStackTrace();
+                error("Failed to parse message due to: %s", e);
+                try {
+                    this.conn.close();
+                } catch (IOException ex) {
+                    error("Failed to close socket due to: %s", e);
+                }
+                return;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                error("Failed to send response due to: %s", e);
+                // Hopefully this will be fixed later one.
+            }
         }
     }
 
 
-    /**
-     * Reads the header from the input stream.
-     * @param in - the input strema
-     * @return An {@link AuthServerRequestHeader} or null in case of an error.
-     */
-    private AuthServerRequestHeader readHeader(BufferedInputStream in) {
-        try {
-            byte[] headerBytes = new byte[Constants.REQUEST_HEADER_SIZE];
-            int readBytes = in.read(headerBytes);
-
-            /*
-             * -1 is required here because there might be a request with only 23 bytes (just the header, with 0 payload)
-             */
-            if (readBytes != Constants.REQUEST_HEADER_SIZE && readBytes != -1) {
-                error("Failed to read header, expected 23 bytes but got %d", readBytes);
-                return null;
-            }
-
-            return AuthServerRequestHeader.parseHeader(headerBytes);
-        } catch (IOException e) {
-            error("Failed to read request header from input stream due to: %s", e);
-            return null;
-        } catch (InvalidRequestException e) {
-            error("%s", e);
-            return null;
-        }
-
-    }
-
-    private AuthServerRequestBody readBody(BufferedInputStream in, int size, RequestCode code){
-        byte[] bodyBytes = new byte[size];
-
-        try{
-            int readBytes = in.read(bodyBytes);
-            if(readBytes != size && readBytes != -1){
-                error("Failed to read body, expected %d bytes, but got %d", readBytes);
-                return null;
-            }
-
-            return AuthServerRequestBody.parse(bodyBytes, code);
-        } catch (IOException e) {
-            error("Failed to read request body from input stream due to: %s", e);
-            return null;
-        } catch (InvalidRequestException e) {
-            error("%s", e);
-            return null;
-        }
-
-    }
 }
