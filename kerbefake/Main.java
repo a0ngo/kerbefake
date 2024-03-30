f2b381bb4f60af3aa7540884f3d41ff435868aef
a0ngo <106151463+a0ngo@users.noreply.github.com>
2024-03-30 20:37:27 +0300

    chore: updated main to properly get data from user

--- kerbefake/Main.java
+++ kerbefake/Main.java
@@ -1,50 +1,55 @@
 package kerbefake;
 
 import kerbefake.auth_server.AuthServer;
+import kerbefake.client.Client;
 import kerbefake.common.Constants;
 import kerbefake.msg_server.MessageServer;
 
 import java.io.IOException;
 import java.util.Scanner;
 
-import static kerbefake.common.Logger.error;
-import static kerbefake.common.Logger.print;
+import static kerbefake.common.Logger.*;
 
 public class Main {
 
     public static void main(String[] args) {
         int modeSelection = -1;
 
+        Thread authServerThread = null, msgServerThread = null;
         Scanner userInputScanner = new Scanner(System.in);
-        print("Please select mode of operation:\n1) Client mode\n2) Auth Server\n3) Messaging Server");
-
-        do {
-            try {
+        try {
+            do {
+                print("Please select mode of operation:\n1) Client mode\n2) Auth Server\n3) Messaging Server");
                 modeSelection = userInputScanner.nextInt();
-                if (modeSelection > 3 || modeSelection < 1) {
-                    throw new Exception();
+                switch (modeSelection) {
+                    case Constants.MODE_CLIENT:
+                        new Client().run();
+                        return;
+                    case Constants.MODE_AUTH:
+                        AuthServer authServer = new AuthServer();
+                        authServerThread = new Thread(authServer::start);
+                        authServerThread.start();
+                        break;
+                    case Constants.MODE_SERVER:
+                        try {
+                            MessageServer msgServer = new MessageServer();
+                            msgServerThread = new Thread(msgServer::start);
+                            msgServerThread.start();
+                        } catch (IOException e) {
+                            error("Failed to start message server due to: %s", e);
+                            return;
+                        }
+                        break;
+                    default:
+                        info("Provided invalid input, exiting");
+                        return;
                 }
-                break;
-            } catch (Exception e) {
-                error("Invalid input, please provide a number (1/2/3).");
-            }
-        } while (true);
-
-        switch (modeSelection) {
-            case Constants.MODE_CLIENT:
-                break;
-            case Constants.MODE_AUTH:
-                new AuthServer();
-                break;
-            case Constants.MODE_SERVER:
-                try {
-                    MessageServer server = new MessageServer();
-                    new Thread(server::start).start();
-                } catch (IOException e) {
-                    error("Failed to start message server due to: %s", e);
-                    return;
-                }
-                break;
+            } while (true);
+        } finally {
+            if (authServerThread != null)
+                authServerThread.interrupt();
+            if (msgServerThread != null)
+                msgServerThread.interrupt();
         }
 
     }
